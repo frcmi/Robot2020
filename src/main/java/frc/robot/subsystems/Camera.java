2660f81759023e40bff36ac8b98f815c93793d18
Walt McKelvie <waltmckel@gmail.com>
2020-02-22 02:20:29 -0800

    Theoretically working NetworkTables

--- src/main/java/frc/robot/subsystems/Camera.java
+++ src/main/java/frc/robot/subsystems/Camera.java
@@ -8,6 +8,10 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
+import edu.wpi.first.wpilibj.geometry.Pose2d;
+import edu.wpi.first.wpilibj.geometry.Rotation2d;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.Solenoid;
 
@@ -15,15 +19,17 @@ import edu.wpi.first.wpilibj.Solenoid;
  * Add your docs here.
  */
 public class Camera extends Subsystem {
-  public static class CameraConstants{
+  public static class CameraConstants {
     public static final int pcmID = 11;
     public static final int pcmPort = 7;
   }
 
   Solenoid light;
+  NetworkTable opsi;
 
-  public Camera(){
+  public Camera() {
     super();
+    opsi = NetworkTableInstance.getDefault().getTable("OpenSight");
     light = new Solenoid(CameraConstants.pcmID, CameraConstants.pcmPort);
     light.set(true);
   }
@@ -36,5 +42,17 @@ public class Camera extends Subsystem {
 
   public void toggle() {
     light.set(!light.get());
+  }
+
+  public Pose2d getPose(){
+    Pose2d p = new Pose2d(opsi.getEntry("position-x").getDouble(0), 
+                          opsi.getEntry("position-y").getDouble(0), 
+                          Rotation2d.fromDegrees(opsi.getEntry("camera-angle").getDouble(0)));
+    
+    if(p.getTranslation().getX() == 0.0 && p.getTranslation().getY() == 0.0){
+      return null;
+    } else{
+      return p;
+    }
   }
 }
